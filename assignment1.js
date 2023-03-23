d3120b9c53f8dd30b1cac88f6af17307836133bd
unknown <bellal1985h@gmail.comm>
2023-03-23 20:12:58 +0600

    test

---
+++ assignment1.js
@@ -0,0 +1,17 @@
+function multiplicationTable(num) {
+  for (let i = 1; i <= 10; i++) {
+    let product = num * i;
+    console.log(`${num} * ${i} = ` + num * i);
+  }
+}
+
+multiplicationTable(1);
+multiplicationTable(2);
+multiplicationTable(3);
+multiplicationTable(4);
+multiplicationTable(5);
+multiplicationTable(6);
+multiplicationTable(7);
+multiplicationTable(8);
+multiplicationTable(9);
+multiplicationTable(10);
